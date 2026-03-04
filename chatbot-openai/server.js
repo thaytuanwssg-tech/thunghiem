127aadc70546ac6f6df0df5f967d95e9ca9c9793
Tuan Nguyen <thaytuanwssg@gmail.com>
2026-03-05 00:35:15 +0700

    Update server.js

--- chatbot-openai/server.js
+++ chatbot-openai/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
-
+console.log("API KEY:", process.env.OPENAI_API_KEY)
 const app = express();
 
 app.use(cors());
